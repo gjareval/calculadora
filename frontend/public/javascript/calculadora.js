3142d8ee9de323fa56a78e30475634473275ec16
gjareval <gjareval@spol.edu.ec>
2023-06-17 13:10:42 -0500

    Cambios finales

--- frontend/public/javascript/calculadora.js
+++ frontend/public/javascript/calculadora.js
@@ -11,8 +11,13 @@ let funcionPredeterminada= () => {
         let interfaz = parseFloat(document.getElementById('interfazHoras').value)
         let html= parseFloat(document.getElementById('htmlHoras').value)
         let css = parseFloat(document.getElementById('cssHoras').value)
+        let js = parseFloat(document.getElementById('jsHoras').value)
+        let apis = parseFloat(document.getElementById('apisHoras').value)
+        let adicionales = parseFloat(document.getElementById('adicionalesHoras').value)
+        let experiencia = parseFloat(document.getElementById('experiencia').value)
 
-        let resultado = parseFloat((valorPorHora * interfaz) + (valorPorHora * html) + (valorPorHora * css))
+        let resultado = parseFloat(((valorPorHora * interfaz) + (valorPorHora * html) + (valorPorHora * css) 
+        + (valorPorHora * js) + (valorPorHora * apis) + (valorPorHora * adicionales))*experiencia)
 
         document.getElementById('valorTotal').value = "$"+resultado
     })
